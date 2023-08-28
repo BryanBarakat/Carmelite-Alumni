55f0e4412ed9b44eddf4837e56c18846dfebb667
bryan barakat <bryan.b@beirut.softsolutions-group.com>
2023-08-28 03:44:29 -0700

    backend to collect data and send emails !

--- send-email.php
+++ send-email.php
@@ -1,52 +1,46 @@
 <?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: *");
-    header('Content-Type: application/json; charset=utf-8');
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-use function PHPSTORM_META\type;
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username = "communications@valoores.com";
+    $mail->Password = "bgqtzkbypbhmkuzm";                         //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    require 'phpmailer/src/Exception.php';
-    require 'phpmailer/src/PHPMailer.php';
-    require 'phpmailer/src/SMTP.php';
-
-    session_start();
-
-    require("connect.php");
-
-    $body = file_get_contents("php://input");
-    $requestData = json_decode($body, true);
-    $data = $requestData[0];
-    $emailTo = $requestData[1];
-
-    // $data = implode(', ', $data);
-
-    $mail = new PHPMailer(true);
-    $mail->isSMTP();
-    $mail->Host = "smtp.gmail.com";
-    $mail->SMTPAuth = true;
-    $mail->Username = "";
-    $mail->Password = "";
-    $mail->SMTPSecure = "ssl";
-    $mail->Port = 465;
-
-    $mail->setFrom("");
-    $mail->addAddress("bryanba02@yahoo.com");
+    //Recipients
+    $mail->setFrom("communications@valoores.com");
+    $mail->addAddress("bryan.barakat@valoores.com");
     $mail->isHTML(true);
     $mail->Subject = 'Carmelite Alumni Data';
     $mail->Body = "ok chief";
 
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
     $mail->send();
-
-    if ($mail) {
-        echo "Email sent successfully";
-    } else {
-        echo "Email sending failed";
-    };
-
-    mysqli_close($connection);
-
-?>
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
